153aeebfa5196a9993ca2e64d9974a7dc14fa20c
hyosikchoi <huchoi24@naver.com>
2024-05-26 17:36:35 +0900

    Add - Dialog sample code practice

--- app/src/main/java/com/hyosik/composepractice/MainActivity.kt
+++ app/src/main/java/com/hyosik/composepractice/MainActivity.kt
@@ -3,13 +3,35 @@ package com.hyosik.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hyosik.composepractice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +44,42 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
+                        var isShow by rememberSaveable {
+                            mutableStateOf(false)
+                        }
+
+                        Button(onClick = { isShow = !isShow }) {
+                            Text(text = "다이얼로그 클릭")
+                        }
+
+                        if(isShow){
+                            Box(
+                                modifier = Modifier
+                                    .width(200.dp)
+                                    .height(120.dp)
+                                    .background(Color.Black.copy(alpha = 0.7f)),
+                                contentAlignment = Alignment.Center
+
+                            ) {
+                                Card(
+                                    shape = RoundedCornerShape(3.dp)
+                                ) {
+                                    Column {
+                                        Text(text = "Hello World")
+                                        Spacer(modifier = Modifier.height(10.dp))
+                                        Text(text = "Test")
+                                    }
+                                }
+                            }
+                        }
+                    }
+
                 }
             }
         }
